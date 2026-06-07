ccb785fa5010db17e78cd5325268f1239dd46673
ngxc <ngoc.shintrann@gmail.com>
2026-06-07 15:37:43 +0700

    chore: auto-sync dotfiles at 2026-06-07 15:37:43

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -9,7 +9,10 @@ return {
   },
   config = function()
     -- Không cần pcall rườm rà, Lazy.nvim đã lo vụ bảo hiểm sinh mạng cho Neovim rồi!
+    local install = require("nvim-treesitter.install")
+    install.ts_generate_args = { "generate", "--abi", tostring(vim.treesitter.language_version) }
     require("nvim-treesitter.configs").setup({
+      prefer_git = true,
       -- Kích hoạt highlight dựa trên AST
       highlight = {
         enable = true,
